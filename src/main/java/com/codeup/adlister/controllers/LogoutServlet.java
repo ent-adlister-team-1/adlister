cd65a9c625eba79686b0295e02f3e1a4f7c41cd8
SamR0429 <Samanatha.ramos01@gmail.com>
2023-09-07 15:35:03 -0500

    added logout page funtionality

--- src/main/java/com/codeup/adlister/controllers/LogoutServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LogoutServlet.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.controllers;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +10,34 @@ import java.io.IOException;
 
 @WebServlet(name = "controllers.LogoutServlet", urlPatterns = "/logout")
 public class LogoutServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        request.getSession().removeAttribute("user");
+//        request.getSession().invalidate();
+//        response.sendRedirect("/login");
+//    }
+//
+////    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+////        request.getSession().removeAttribute("user");
+////        request.getSession().invalidate();
+////        response.sendRedirect("/login");
+////    }
+//
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        request.getSession().removeAttribute("user");
+//        request.getSession().invalidate();
+//        response.sendRedirect("/logout");
+//    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getSession().removeAttribute("user");
         request.getSession().invalidate();
-        response.sendRedirect("/login");
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/logout.jsp");
+        dispatcher.forward(request, response);
     }
+
+
+
+
 }
