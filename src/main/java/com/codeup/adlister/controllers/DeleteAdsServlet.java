7c6d5d0464f3d695a06d30e0fe79391a48ac1ca0
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-09-08 09:33:35 -0500

    added deleteads functionality

---
+++ src/main/java/com/codeup/adlister/controllers/DeleteAdsServlet.java
@@ -0,0 +1,31 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DeleteAdsServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            // add a return statement to exit out of the entire method.
+            return;
+        }
+
+//        request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User loggedInUser = (User) request.getSession().getAttribute("user");
+        Ad ad = (Ad) request.getSession().getAttribute("ad");
+        DaoFactory.getAdsDao().deleteAd(ad);
+
+
+    }
+}
